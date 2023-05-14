fccf9c5457876b84b9eab93c4a876589af84d677
kuasha227 <118634262+kuasha227@users.noreply.github.com>
2023-05-14 23:29:07 +0600

    Update Login.jsx

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
+import { client } from '../client';
 
 const Login = () => {
   const navigate = useNavigate();
@@ -16,6 +17,9 @@ const Login = () => {
       userName: name,
       image: imageUrl,
     };
+    client.createIfNotExists(doc).then(() => {
+      navigate('/', { replace: true });
+    });
   }
   return (
     <div className='flex-justify-start items-center flex-col h-screen'>
@@ -29,11 +33,11 @@ const Login = () => {
           autoPlay
           className="w-full h-full object-cover"
         />
-        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-grey overlay'>
+        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
         <div className='p-5'>
           <img src={logo} width="130px" alt="logo"/>
       </div> 
-      <div className='shadow-2x1'>
+      <div className='shadow-2xl'>
           <GoogleLogin
             clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
             render={(renderProps)=>(
@@ -47,13 +51,13 @@ const Login = () => {
                 )}
                 onSuccess={responseGoogle}
                 onFailure={responseGoogle}
-                cookiePolicy="single-host-origin"
+                cookiePolicy="single_host_origin"
             /> 
             </div>
       </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
