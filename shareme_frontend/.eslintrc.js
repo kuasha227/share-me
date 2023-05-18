f1862daafcaa3e82878dbc29795c39eee8af38bd
kuasha227 <118634262+kuasha227@users.noreply.github.com>
2023-05-18 19:50:57 +0600

    Update .eslintrc.js

--- shareme_frontend/.eslintrc.js
+++ shareme_frontend/.eslintrc.js
@@ -3,10 +3,10 @@ module.exports = {
       browser: true,
       es6: true,
     },
-    "eslintConfig":{extends: [
+    "extends":[
       'plugin:react/recommended',
-      'airbnb',
-    ]},
+      'airbnb'
+    ],
     globals: {
       Atomics: 'readonly',
       SharedArrayBuffer: 'readonly',
