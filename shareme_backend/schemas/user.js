54cc2c7bb4dbf7eaeb4ae4a2d4eb1b434afe6c7b
kuasha227 <118634262+kuasha227@users.noreply.github.com>
2023-05-18 22:50:32 +0600

    Update user.js

--- shareme_backend/schemas/user.js
+++ shareme_backend/schemas/user.js
@@ -1,17 +1,18 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    name : "user",
-    title: "User",
-    type: "document",
+    name: 'user',
+    title: 'User',
+    type: 'document',
     fields: [
-        {
-            name:"username",
-            title:"UserName",
-            type:"string"
-        },
-        {
-            name:"image",
-            title:"Image",
-            type:"image"
-        }
-    ]
-}
+      {
+        name: 'userName',
+        title: 'UserName',
+        type: 'string',
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'string',
+      },
+    ],
+  };
