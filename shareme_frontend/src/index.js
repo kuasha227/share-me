80a1bb0b8f69b8fbf35f8915da82b310e8a52a57
kuasha227 <118634262+kuasha227@users.noreply.github.com>
2023-05-18 17:36:57 +0600

    Update index.js

--- shareme_frontend/src/index.js
+++ shareme_frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import App from "./App";
 import "./index.css";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router} from "react-router-dom";
 
-ReactDOM.render(
+const container= ReactDOM.createRoot(document.getElementById("root"));
+container.render(
     <Router>
         <App />
-    </Router>, 
-    document.getElementById("root")
+    </Router>,
     );
